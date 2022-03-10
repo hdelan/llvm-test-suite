e79c722f1500b6e7bfd5fe929c265bb8cf7f8c2a
Hugh Delaney <hugh.delaney@codeplay.com>
2022-03-10 11:21:15 +0000

    Fix typo

--- SYCL/BFloat16/fma_relu_half_bf16_bf16x2.cpp
+++ SYCL/BFloat16/fma_relu_half_bf16_bf16x2.cpp
@@ -1,4 +1,4 @@
-// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out -Xsycl-target-backend --cuda-gpu-arch-sm_80
+// RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple %s -o %t.out -Xsycl-target-backend --cuda-gpu-arch=sm_80
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 //
 // Only cuda backend implements bf16
